3f15ba50df17f00bb992f2dd574362af87b8c17c
HhGiangg <hhgiang.17it2@sict.udn.vn>
2019-09-19 14:06:10 +0700

    buoi 19

---
+++ session 19/tiendien.js
@@ -0,0 +1,12 @@
+function tinhTienDien() {
+    var name, first_date, last_date, first_number, last_number;
+    name = document.getElementById('name').value;
+    name = document.getElementById('first_date').value;
+    name = document.getElementById('last_date').value;
+    name = document.getElementById('first_number').value;
+    name = document.getElementById('last_number').value;
+
+    if (name == '') {
+
+    }
+}
