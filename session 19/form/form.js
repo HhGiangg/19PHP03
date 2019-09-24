29d05139b6be2fbc00a3e6bf6b2eff438bae8ab3
HhGiangg <hhgiang.17it2@sict.udn.vn>
2019-09-24 18:28:52 +0700

    bvn

---
+++ session 19/form/form.js
@@ -0,0 +1,6 @@
+function check() {
+    var name = document.forms["resginter"]["fullname"].value;
+    var gender = document.forms["resginter"]["gender"].value;
+    var birthday = document.forms["resginter"]["birthday"].value;
+    var city = document.forms["resginter"]["city"].value;
+    var error = false;
